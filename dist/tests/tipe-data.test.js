37b2e338b43afe8c89fe6b4f9afaf1b3a715ae60
tanto <tantows001@gmail.com>
2024-01-08 08:53:04 +0700

    Tipe Data

---
+++ dist/tests/tipe-data.test.js
@@ -0,0 +1,11 @@
+"use strict";
+describe('Data Type', function () {
+    it('should must declar', function () {
+        let name = "Tantowi Shah Hanif";
+        const balace = 1000;
+        const isVip = true;
+        console.log(name);
+        console.log(balace);
+        console.log(isVip);
+    });
+});
---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function(){
+    it('should must declar', function(){
+        let name: String = "Tantowi Shah Hanif"
+        const balace: number = 1000
+        const isVip: boolean = true
+
+        console.log(name)
+        console.log(balace)
+        console.log(isVip)
+    })
+})
